1d32e9ff3d8308f48553ab35b4ce32b10063b3de
MelnykDanilPepsi <da.melnik2011@gmail.com>
2022-02-10 17:47:37 +0200

    first commit

---
+++ ChatClient/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static string userName = "";
+        static TcpClient client = new TcpClient();
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            
+            for(; ;) {
+
+                Console.Clear();
+                Console.Write("Enter your name: ");
+                userName = Console.ReadLine() ?? "";
+
+                if(userName.Length > 0) break;
+            }
+            try
+            {
+                client.Connect("192.168.31.1", 22); 
+                stream = client.GetStream();
+
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+
+                Console.WriteLine("Welcome, {0}", userName);
+                SendMessage();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+
+
+        static void SendMessage() {
+
+            Console.WriteLine("Enter message: ");
+
+            while (true) {
+
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+        static void ReceiveMessage() {
+            while (true) {
+                try {
+                    byte[] data = new byte[64];
+                    int bytes = 0;
+
+                    do {
+
+                     bytes = stream.Read(data, 0, data.Length);
+                     new StringBuilder().Append(Encoding.Unicode.GetString(data, 0, bytes));
+
+                    } while (stream.DataAvailable);
+
+
+                    string message = new StringBuilder().ToString();
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("Error, please try again.");
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            try
+            {
+                if (stream != null)
+                    stream.Close();
+
+                if (client != null)
+                    client.Close();
+
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+        }
+    }
+}
---
+++ ChatSocket/ClientObject .cs
@@ -0,0 +1,93 @@
+﻿    using System;
+    using System.Net.Sockets;
+    using System.Text;
+
+    namespace ChatSocket
+{
+        public class ClientObject
+        {
+            protected internal string Id { get; private set; }
+            protected internal NetworkStream Stream { get; private set; }
+            string userName;
+            TcpClient client;
+            ServerObject server; 
+
+            public ClientObject(TcpClient tcpClient, ServerObject serverObject) {
+
+                Id = Guid.NewGuid().ToString();
+                client = tcpClient;
+                server = serverObject;
+                serverObject.AddConnection(this);
+            }
+
+            public void Process()
+            {
+                try
+                {
+                    Stream = client.GetStream();
+                    string message = GetMessage();
+                    userName = message;
+
+                    message = userName + "has entered the chat.";
+                    server.BroadcastMessage(message, this.Id);
+                    Console.WriteLine(message);
+                    while (true)
+                    {
+                        try
+                        {
+                            message = GetMessage();
+                            message = String.Format("{0}: {1}", userName, message);
+
+                            Console.WriteLine(message);
+                            server.BroadcastMessage(message, this.Id);
+                        }
+                        catch
+                        {
+                            message = String.Format("{0}: leave chat", userName);
+                            Console.WriteLine(message);
+
+                            server.BroadcastMessage(message, this.Id);
+                            break;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    server.RemoveConnection(this.Id);
+                    Close();
+                }
+            }
+            private string GetMessage()
+            {
+                byte[] data = new byte[64]; 
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = Stream.Read(data, 0, data.Length);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+
+                while (Stream.DataAvailable);
+
+                return builder.ToString();
+            }
+
+            protected internal void Close()
+            {
+               try {
+
+                if (Stream != null) Stream.Close();
+                if (client != null) client.Close();
+
+                }
+                catch(Exception ex) { Console.WriteLine($"{ex}"); }
+            }
+        }
+    }
+
